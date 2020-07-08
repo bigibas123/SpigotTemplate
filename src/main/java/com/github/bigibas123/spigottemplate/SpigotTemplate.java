5a1d1e4a0bff60694074a4c4092cabfb43cd8a20
Bas Dingemans <sebastiaandingemans@gmail.com>
2020-07-09 01:15:12 +0200

    Created base files

---
+++ src/main/java/com/github/bigibas123/spigottemplate/SpigotTemplate.java
@@ -0,0 +1,21 @@
+package com.github.bigibas123.spigottemplate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpigotTemplate extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
